cb13a4aa921331fb98e3a3233b1c0817f486d03b
adifdwimaulana <adifdwimaulana@gmail.com>
2024-11-21 20:25:34 +0700

    fix: remove dfinity auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ActorProvider, AgentProvider } from '@ic-reactor/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AgentProvider withProcessEnv>
-      <App />
-    </AgentProvider>
+    <App />
   </React.StrictMode>,
 );
