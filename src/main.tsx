bb9fbc07cbcf7cc2f7aa7fdec985d5efa57cdd74
adifdwimaulana <adifdwimaulana@gmail.com>
2024-11-21 20:23:11 +0700

    fix: remove dfx files

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ActorProvider, AgentProvider } from '@ic-reactor/react';
-import { idlFactory, canisterId } from './declarations/backend';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AgentProvider withProcessEnv>
-      <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
-        <App />
-      </ActorProvider>
+      <App />
     </AgentProvider>
   </React.StrictMode>,
 );
