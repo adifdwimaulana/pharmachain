b48d3d96a75a763527ab0b57a86ae18a1d518cd7
adifdwimaulana <adifdwimaulana@gmail.com>
2024-11-21 20:27:23 +0700

    fix: remove ic-reactor hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import LandingPage from '@/pages/landing-page';
-import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const { data: count, call: refetchCount } = useQueryCall({
-    functionName: 'get',
-  });
-
-  const { call: increment, loading } = useUpdateCall({
-    functionName: 'inc',
-    onSuccess: () => {
-      refetchCount();
-    },
-  });
-
   return (
     <BrowserRouter>
       <Routes>
